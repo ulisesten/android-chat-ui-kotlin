deaf7fd44367d306eef831db0f31503aa8a701b7
ulisesten <ulises.mtz.el@gamil.com>
2021-03-12 15:52:13 -0600

    first commit

---
+++ app/src/main/java/com/example/messaging_ui/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.messaging_ui
+
+import Adapter
+import Message
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val messages: MutableList<Message> = mutableListOf()
+    var adapter: Adapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val userId = "test_id_1"
+
+        if (adapter == null) {
+            adapter = Adapter(userId!!, messages)
+        }
+
+        message_recycler_view.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false )
+        message_recycler_view.adapter = adapter
+
+
+        send_message_button.setOnClickListener {
+            val message = text_message_input.text.toString()
+
+            if (message.isEmpty()) {
+                //Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            /**(val fromId: String,
+            val toId: String,
+            val senderName: String,
+            val receiverName:String,
+            val message: String,
+            val productId:String,
+            val time:String,
+            val fromDeviceId:String?)*/
+
+            messages.add(Message(   userId
+                                 , "receiver_id"
+                                 , "sender_name"
+                                 , "receiver_name"
+                                 ,  message
+                                 , "product_id"
+                                 , "'2012-11-04T14:51:06.157Z'"
+                                 , "from_device_daf87as9f4"))
+
+
+            text_message_input.text.clear()
+
+            adapter!!.notifyDataSetChanged()
+
+        }
+    }
+    
+}
---
+++ app/src/main/java/com/example/messaging_ui/messaging/Adapter.kt
@@ -0,0 +1,88 @@
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.example.messaging_ui.R
+import com.example.messaging_ui.utils.Utils
+import kotlinx.android.synthetic.main.incoming_message.view.*
+
+data class Message(val fromId: String,
+                   val toId: String,
+                   val senderName: String,
+                   val receiverName:String,
+                   val message: String,
+                   val productId:String,
+                   val time:String,
+                   val fromDeviceId:String?)
+
+class MyViewHolder(v: View): RecyclerView.ViewHolder(v)
+
+class Adapter (private val userId:String, private val elements: MutableList<Message>): RecyclerView.Adapter<MyViewHolder>() {
+
+    private val viewTypeMessageSent = 1
+    private val viewTypeMessageReceived = 2
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val layoutInflater: LayoutInflater
+        var cellForRow: View? = null
+
+        if( viewType == viewTypeMessageSent ) {
+            layoutInflater = LayoutInflater.from(parent.context)
+            cellForRow =
+                layoutInflater.inflate(R.layout.outgoing_message, parent, false)
+        }
+        else if ( viewType == viewTypeMessageReceived ){
+            layoutInflater = LayoutInflater.from(parent.context)
+            cellForRow =
+                layoutInflater.inflate(R.layout.incoming_message, parent, false)
+        }
+
+        return MyViewHolder(cellForRow!!)
+
+    }
+
+    override fun getItemCount(): Int {
+        return elements.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+
+        val message = elements[position]
+
+        return if( message.fromId == userId ) viewTypeMessageSent
+        else viewTypeMessageReceived
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, pos: Int) {
+
+        val parsedTime = Utils().parseTimestamp(elements[pos].time)
+
+        holder.itemView.text_message_body.text = elements[pos].message
+        holder.itemView.text_message_time.text = parsedTime[1]
+
+        if( holder.itemViewType == viewTypeMessageReceived){
+
+            holder.itemView.text_message_name.text = elements[pos].senderName
+
+            val imgView = holder.itemView.image_message_profile
+
+            val mContext = holder.itemView.context
+
+            //https://res.cloudinary.com/djlzeapiz/image/upload/v1576169609/xqV0FWz7elPsZP3sG0otw.jpg
+            //https://res.cloudinary.com/djlzeapiz/image/upload/v1576169609/xqV0FWz7elPsZP3sG0otw.jpg
+            //Glide.with(mContext).load("https://res.cloudinary.com/djlzeapiz/image/upload/q_10/v1573315852/" + elementos[pos].fromId)
+            //    .apply(RequestOptions.circleCropTransform()).into(imgView)
+
+            holder.itemView.setOnClickListener {
+
+                Toast.makeText(mContext, "To Implement", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+}
+
